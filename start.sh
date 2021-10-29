4a22702d801f6e0865e7311c53330e38e4e969ea
slcnx <35824750+slcnx@users.noreply.github.com>
2021-10-29 12:42:17 +0800

    Update start.sh
    添加对mqtt-插件的认证, 支持环境变量如下
    RABBITMQ_MQTT_VHOST 将插件用户添加到哪个VHOST
    RABBITMQ_MQTT_USER RABBITMQ_MQTT_PASSWORD 插件的用户及密码
    
    连接mqtt时，使用 
    用户名:   $RABBITMQ_MQTT_VHOST:$RABBITMQ_MQTT_USER    
    密码：    $RABBITMQ_MQTT_PASSWORD

--- start.sh
+++ start.sh
@@ -6,19 +6,25 @@ set -e
 count=0;
 # Execute list_users until service is up and running
 until timeout 5 rabbitmqctl list_users >/dev/null 2>/dev/null || (( count++ >= 60 )); do sleep 1; done;
-if rabbitmqctl list_users | grep guest > /dev/null
-then
-   # Delete default user and create new users
-   rabbitmqctl delete_user guest
-   # Specific commands for Application
-   rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD 2>/dev/null ; \
-   rabbitmqctl set_user_tags $RABBITMQ_USER administrator ; \
-   rabbitmqctl set_permissions -p / $RABBITMQ_USER  ".*" ".*" ".*" ; \
+   if rabbitmqctl list_users | grep guest > /dev/null
+   then
+      # Delete default user and create new users
+      rabbitmqctl delete_user guest
+      # Specific commands for Application
+      rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD 2>/dev/null ; \
+      rabbitmqctl set_user_tags $RABBITMQ_USER administrator ; \
+      rabbitmqctl set_permissions -p / $RABBITMQ_USER  ".*" ".*" ".*" ; \
 
-   echo "setup completed"
-else
-   echo "already setup"
-fi
+      # add virtualhost and create new uesrs 
+      rabbitmqctl add_vhost $RABBITMQ_MQTT_VHOST; \
+      rabbitmqctl add_user $RABBITMQ_MQTT_USER $RABBITMQ_MQTT_PASSWORD; \
+      rabbitmqctl set_user_tags $RABBITMQ_MQTT_USER management; \
+      rabbitmqctl set_permissions -p $RABBITMQ_MQTT_VHOST $RABBITMQ_MQTT_USER ".*" ".*" ".*" ;  \
+
+      echo "setup completed"
+   else
+      echo "already setup"
+   fi
 ) &
 
 # Call original entrypoint
